299787605aeb5d69c893df9e4b0c0e0a5698384b
Ciprian-Octavian Truică <ciprian.octavian.truica@gmail.com>
2023-03-03 15:55:51 +0200

    Update mcwdst.py

--- mcwdst.py
+++ mcwdst.py
@@ -1,3 +1,13 @@
+# coding: utf-8
+
+__author__      = "Ciprian-Octavian Truică, Elena-Simona Apostol"
+__copyright__   = "Copyright 2021, University Politehnica of Bucharest"
+__license__     = "GNU GPL"
+__version__     = "0.1"
+__email__       = "{ciprian.truica,elena.apostol}@upb.ro"
+__status__      = "Development"
+
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import tree_mitigator
